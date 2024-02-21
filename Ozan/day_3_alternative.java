cd5823c19a81fa50c87677d9f7f889d6daf862d6
Ozan Cinci <ozan_cinci2001@hotmail.com>
2024-02-21 17:50:04 +0300

    day 3 - ozan alternative solution

---
+++ Ozan/day_3_alternative.java
@@ -0,0 +1,33 @@
+// Solution for 78. Subsets
+// link: https://leetcode.com/problems/subsets/description/
+
+
+// ALTERNATIVE SOLUTION, WORKS SLOWER THAN MAIN SOLUTION
+// BUT I LIKED THE WAY OF THINKING
+class Solution {
+    public List<List<Integer>> subsets(int[] nums) {
+        List<List<Integer>> result = new ArrayList<>();
+        for (int i=0 ; i<Math.pow(2,nums.length)  ;i++)
+            result.add(new ArrayList<>());
+
+        int stepCounter = result.size()/2;
+        for (int number : nums) {
+            int writeCounter = stepCounter;
+            int waitCounter = 0;
+            for (int index=0; index<result.size(); index++) {
+                if (waitCounter==0) {
+                    result.get(index).add(number);
+                    writeCounter -= 1;
+                    if (writeCounter==0)
+                        waitCounter=stepCounter;
+                } else {
+                    waitCounter -= 1;
+                    writeCounter = stepCounter;
+                }
+            }
+            stepCounter/=2;
+        }
+
+        return result;
+    }
+}
