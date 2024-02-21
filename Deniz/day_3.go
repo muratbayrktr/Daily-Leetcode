ff299d34e9722d9deb07468c75003f6ff18f3f7f
DENIZCAN YILMAZ <2444172@ceng.metu.edu.tr>
2024-02-21 18:05:50 +0300

    deniz day3 with go

---
+++ Deniz/day_3.go
@@ -0,0 +1,29 @@
+package main
+
+import "fmt"
+
+func subsets(nums []int) [][]int {
+    result := [][]int{{}}
+    
+    for _, num := range nums {
+        currentSize := len(result)
+        for i := 0; i < currentSize; i++ {
+            newSubset := make([]int, len(result[i]), len(result[i])+1)
+            copy(newSubset, result[i])
+            newSubset = append(newSubset, num)
+
+            result = append(result, newSubset)
+        }
+    }
+    
+    return result
+}
+
+func main() {
+    nums := []int{1, 2, 3}
+    subsetsResult := subsets(nums)
+
+    for _, subset := range subsetsResult {
+        fmt.Println(subset)
+    }
+}
